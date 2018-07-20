3d996e72dc64b0a8f410a49f4347f48f43db30a6
rohitb <rohitb@PUNHWD11671.corp.capgemini.com>
2018-07-20 09:57:56 +0530

    Initial Commit

---
+++ WebContent/WEB-INF/AdmissionForm.jsp
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+	<h1>${headerMessage}</h1>
+
+	<h3> STUDENT ADMISSION FORM FOR ENGINEERING COURSES</h3>
+
+	<form action="/FirstSpringMVCProject/submitAdmissionForm.html" method="post">
+		<p>
+			Student's Name : <input type="text" name="studentName" />
+		</p>
+		<p>
+			Student's Hobby : <input type="text" name="studentHobby" />
+		</p>
+		
+		<input type="submit" value="Submit this form by clicking here" />
+	</form>
+ 
+</body>
+</html>
+
---
+++ src/com/gontuseries/studentadmissioncontroller/StudentAdmissionController.java
@@ -0,0 +1,37 @@
+package com.gontuseries.studentadmissioncontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StudentAdmissionController {
+
+
+	@RequestMapping(value="/admissionForm.html", method = RequestMethod.GET)
+	public ModelAndView getAdmissionForm() {
+
+		ModelAndView model1 = new ModelAndView("AdmissionForm");
+		
+		return model1;
+	}
+
+	@ModelAttribute
+    public void addingCommonObjects(Model model1) {
+		
+		model1.addAttribute("headerMessage", "Gontu College of Engineering, India");
+	}
+
+	@RequestMapping(value="/submitAdmissionForm.html", method = RequestMethod.POST)
+	public ModelAndView submitAdmissionForm(@ModelAttribute("student1") Student student1) {
+
+
+		ModelAndView model1 = new ModelAndView("AdmissionSuccess");
+		return model1;
+	}
+	
+}
+
